b9ac9ac459cbcf3606123f04e5d72b6b04336166
Aaimry <wheal28@gmail.com>
2021-12-23 17:34:11 +0600

    Изинлила path в файле urls.py

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todolist.views import todolist_view
+from todolist.views import todolist_view, create_todolist_view
 
 urlpatterns = [
-    path('/', todolist_view)
+    path('', todolist_view),
+    path('add/', create_todolist_view)
     ]
