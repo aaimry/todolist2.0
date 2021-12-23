743e09e53b6c6b18ccb6eb136eb77abfd3c5ce5f
Aaimry <wheal28@gmail.com>
2021-12-23 15:03:13 +0600

    Модель ToDoList Зарегистрирована в панели администратора

--- source/todolist/admin.py
+++ source/todolist/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from todolist.models import ToDoList
+
+
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'aim', 'status', 'deadline_at']
+    list_filter = ['aim']
+    search_fields = ['aim', 'status']
+    fields = ['aim', 'status', 'deadline_at']
+
+
+admin.site.register(ToDoList, ToDoListAdmin)
