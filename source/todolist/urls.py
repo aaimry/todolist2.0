8f0cd8fe522039b91bf90fe20d26eb97e60c169d
Aaimry <wheal28@gmail.com>
2021-12-30 18:40:12 +0600

    Изменили pk задач с квери параметра на переменную пути

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -4,5 +4,5 @@ from todolist.views import todolist_view, create_todolist_view, check_list_view
 urlpatterns = [
     path('', todolist_view),
     path('add/', create_todolist_view),
-    path('check/', check_list_view)
+    path('check/<int:pk>', check_list_view)
     ]
--- source/todolist/views.py
+++ source/todolist/views.py
@@ -19,11 +19,11 @@ def create_todolist_view(request):
         new_aim = ToDoList.objects.create(aim=aim, status=status, deadline_at=deadline_at)
         # context = {"aim_list": new_aim}
         # return render(request, 'todolist_create.html', context)
-        return redirect(f'/check/?pk={new_aim.pk}')
+        return redirect(f'/check/{new_aim.pk}')
 
 
-def check_list_view(request):
-    pk = request.GET.get('pk')
+def check_list_view(request, pk):
+    # pk = request.GET.get('pk')
     check_list = ToDoList.objects.get(pk=pk)
     context = {'aim_list': check_list}
     return render(request, 'check_list.html', context)
