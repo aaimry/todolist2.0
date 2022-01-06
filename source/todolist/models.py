8d78f45efeb7a832a0bcff533f776981adc60142
Aaimry <wheal28@gmail.com>
2022-01-06 18:09:49 +0600

    в models.py изменила переменную status_choice

--- source/todolist/models.py
+++ source/todolist/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
 
 class ToDoList(models.Model):
-    new = 'new'
-    in_progress = 'in_progress'
-    done = 'done'
-    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
     aim = models.CharField(max_length=200, null=False, blank=False, verbose_name='Задача')
-    status = models.CharField(max_length=11, choices=status_choices, default='new', verbose_name='Статус')
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Подробное описание')
     deadline_at = models.DateField(null=True, blank=True, verbose_name='Дедлайн')
 
