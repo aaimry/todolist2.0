6d6f128c7cb0619f0f26d516590e2f4725edf7db
Aaimry <wheal28@gmail.com>
2022-01-06 15:15:19 +0600

    Создала редактирование и удаление во views.py,зарегистрировала uri в urls.py, cоздала шаблоны для редактирования и удаления

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from todolist.views import todolist_view, create_todolist_view, check_list_view
+from todolist.views import todolist_view, create_todolist_view, check_list_view, update_list_view, delete_list_view
 
 urlpatterns = [
     path('', todolist_view, name='index'),
     path('add/', create_todolist_view, name='list_add'),
-    path('check/<int:pk>/', check_list_view, name='list_check')
-    ]
+    path('check/<int:pk>/', check_list_view, name='list_check'),
+    path('check/<int:pk>/update', update_list_view, name='list_update'),
+    path('check/<int:pk>/delete', delete_list_view, name='list_delete')
+]
--- source/todolist/views.py
+++ source/todolist/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponseNotFound
+
+from forms import ToDoListForm
 from todolist.models import ToDoList
 from django.urls import reverse
 
@@ -21,7 +23,7 @@ def create_todolist_view(request):
         if deadline_at == '':
             deadline_at = None
         if description == '':
-            description = None
+            description = 'Отсутстувет'
         new_aim = ToDoList.objects.create(aim=aim, status=status, deadline_at=deadline_at, description=description)
         return redirect('list_check', pk=new_aim.pk)
 
@@ -33,3 +35,27 @@ def check_list_view(request, pk):
         return HttpResponseNotFound('Задача не найдена')
     context = {'aim_list': check_list}
     return render(request, 'check_list.html', context)
+
+
+def update_list_view(request, pk):
+    aim_list = get_object_or_404(ToDoList, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'updatelist.html', {'aim_list': aim_list})
+    else:
+        aim_list.aim = request.POST.get('aim')
+        aim_list.status = request.POST.get('status')
+        aim_list.deadline_at = request.POST.get('deadline_at')
+        aim_list.description = request.POST.get('description')
+        aim_list.save()
+        return redirect('check_list_view', pk=aim_list.pk)
+
+
+def delete_list_view(request, pk):
+    aim_list = ToDoList.objects.get(pk=pk)
+    if request.method == 'GET':
+        return render(request, 'todolist_delete.html', {'aim_list': aim_list})
+    else:
+        aim_list.delete()
+        return redirect('index')
+
+
