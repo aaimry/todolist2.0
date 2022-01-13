e64755c5669d1b0477eff0255d7bf325b74c9bac
Aaimry <wheal28@gmail.com>
2022-01-13 18:32:03 +0600

    создала модель для типа и статуса, зарегистрировала в админке

--- source/todolist/admin.py
+++ source/todolist/admin.py
@@ -1,14 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
-from todolist.models import ToDoList
+
+from todolist.models import ToDoList, StatusChoice, TypeChoice
 
 
 class ToDoListAdmin(admin.ModelAdmin):
-    list_display = ['id', 'aim', 'status', 'deadline_at']
+    list_display = ['id', 'aim', 'type', 'status']
     list_filter = ['aim']
     search_fields = ['aim', 'status']
-    fields = ['aim', 'status', 'description', 'deadline_at']
+    fields = ['aim', 'description', 'type',  'status']
 
 
 admin.site.register(ToDoList, ToDoListAdmin)
+
+
+class StatusChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status_obj']
+    list_filter = ['status_obj']
+    search_fields = ['status_obj']
+    fields = ['status_obj']
+
+
+admin.site.register(StatusChoice, StatusChoiceAdmin)
+
+
+class TypeChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type_obj']
+    list_filter = ['type_obj']
+    search_fields = ['type_obj']
+    fields = ['type_obj']
+
+
+admin.site.register(TypeChoice, TypeChoiceAdmin)
--- source/todolist/models.py
+++ source/todolist/models.py
@@ -1,19 +1,35 @@
 from django.db import models
 
+TYPE_CHOICES = [('task', 'Задача'), ('bug', 'Баг'), ('enhancement', 'Улучшение')]
 STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
+class StatusChoice(models.Model):
+    status_obj = models.CharField(max_length=11, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
+
+    def __str__(self):
+        return f"{self.status_obj}"
+
+
+class TypeChoice(models.Model):
+    type_obj = models.CharField(max_length=12, null=False, choices=TYPE_CHOICES, default='task',  verbose_name='Тип')
+
+    def __str__(self):
+        return f"{self.type_obj}"
 
 
 class ToDoList(models.Model):
     aim = models.CharField(max_length=200, null=False, blank=False, verbose_name='Задача')
-    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Подробное описание')
-    deadline_at = models.DateField(null=True, blank=True, verbose_name='Дедлайн')
+    type = models.ForeignKey('todolist.TypeChoice', on_delete=models.PROTECT, default='task', related_name='type', verbose_name='Тип')
+    status = models.ForeignKey('todolist.StatusChoice', on_delete=models.PROTECT, default='new', related_name='status', verbose_name='Статус')
+    create_date = models.DateTimeField(null=False, auto_now=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(null=False, auto_now=True, verbose_name='Дата редактировнаия')
 
     def __str__(self):
-        return f"{self.aim} {self.status} {self.deadline_at}{self.description}"
+        return f"{self.aim}{self.description}{self.type}{self.status}"
 
-
-class Meta:
-    db_table = 'ToDoList'
-    verbose_name = 'Список дел'
-    verbose_name_plural = 'Список дел'
+    class Meta:
+        db_table = 'ToDoList'
+        verbose_name = 'Список дел'
+        verbose_name_plural = 'Списки дел'
