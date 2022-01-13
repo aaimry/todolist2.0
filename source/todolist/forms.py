d9631616eb279cfe07ba5859924edd743a9b1e43
Aaimry <wheal28@gmail.com>
2022-01-13 18:37:17 +0600

    переместила forms.py в приложение todolist, в нем изменила поля формы

--- source/todolist/forms.py
+++ source/todolist/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from todolist.models import STATUS_CHOICES
+from todolist.models import StatusChoice, TypeChoice
 
 
 class ToDoListForm(forms.Form):
     aim = forms.CharField(max_length=200, required=True, label='Задача')
-    deadline_at = forms.CharField(required=False, label='Дедлайн')
-    status = forms.ChoiceField(choices=STATUS_CHOICES, required=True, label='Cтатус', initial=STATUS_CHOICES[0][1])
     description = forms.CharField(max_length=2000, required=False, label='Описание',
                                   widget=forms.Textarea())
+    type = forms.ModelChoiceField(queryset=TypeChoice.objects.all(), label='Тип')
+    status = forms.ModelChoiceField(queryset=StatusChoice.objects.all(), label='Статус')
