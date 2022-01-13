2d2b749432ef2385baacd3dbd3579a2fe1af781a
Aaimry <wheal28@gmail.com>
2022-01-13 18:42:36 +0600

    для типа и статуса задала дефолтные значения

--- source/todolist/forms.py
+++ source/todolist/forms.py
@@ -6,5 +6,5 @@ class ToDoListForm(forms.Form):
     aim = forms.CharField(max_length=200, required=True, label='Задача')
     description = forms.CharField(max_length=2000, required=False, label='Описание',
                                   widget=forms.Textarea())
-    type = forms.ModelChoiceField(queryset=TypeChoice.objects.all(), label='Тип')
-    status = forms.ModelChoiceField(queryset=StatusChoice.objects.all(), label='Статус')
+    type = forms.ModelChoiceField(queryset=TypeChoice.objects.all(), initial='task', label='Тип')
+    status = forms.ModelChoiceField(queryset=StatusChoice.objects.all(), initial='new',  label='Статус')
