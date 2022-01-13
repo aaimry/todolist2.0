01aedc826273b6fc4069303f9e379f3e98c66869
Aaimry <wheal28@gmail.com>
2022-01-13 18:38:13 +0600

    во views.py для crud создала классы, вызвала их в urls.py

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from todolist.views import todolist_view, create_todolist_view, check_list_view, update_list_view, delete_list_view
+from todolist.views import IndexView, CheckListView, DeleteView, UpdateView, CreateView
 
 urlpatterns = [
-    path('', todolist_view, name='index'),
-    path('add/', create_todolist_view, name='list_add'),
-    path('check/<int:pk>/', check_list_view, name='list_check'),
-    path('check/<int:pk>/update', update_list_view, name='list_update'),
-    path('check/<int:pk>/delete', delete_list_view, name='list_delete')
+    path('', IndexView.as_view(), name='index'),
+    path('add/', CreateView.as_view(), name='list_add'),
+    path('check/<int:pk>/', CheckListView.as_view(), name='list_check'),
+    path('check/<int:pk>/update', UpdateView.as_view(), name='list_update'),
+    path('check/<int:pk>/delete', DeleteView.as_view(), name='list_delete')
 ]
--- source/todolist/views.py
+++ source/todolist/views.py
@@ -1,63 +1,64 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseNotFound
+from django.views import View
+from django.views.generic import TemplateView
 
-from forms import ToDoListForm
+from todolist.forms import ToDoListForm
 from todolist.models import ToDoList
 
 
-def todolist_view(request):
-    aim_list = ToDoList.objects.all()
-    return render(request, 'todolist.html', {'aim_list': aim_list})
+class IndexView(View):
+    def get(self, request):
+        aim_list = ToDoList.objects.all()
+        return render(request, 'todolist.html', {'aim_list': aim_list})
 
 
-def create_todolist_view(request):
-    if request.method == 'GET':
+class CreateView(View):
+    def get(self, request):
         form = ToDoListForm()
         return render(request, 'todolist_create.html', {'form': form})
-    else:
+
+    def post(self, request):
         form = ToDoListForm(data=request.POST)
         if form.is_valid():
             aim = form.cleaned_data.get('aim')
             status = form.cleaned_data.get('status')
-            deadline_at = form.cleaned_data.get('deadline_at')
+            type = form.cleaned_data.get('type')
             description = form.cleaned_data.get('description')
-            if deadline_at == '':
-                deadline_at = None
             if description == '':
                 description = 'Отсутстувет'
-            new_aim = ToDoList.objects.create(aim=aim, status=status, deadline_at=deadline_at, description=description)
+            new_aim = ToDoList.objects.create(aim=aim, description=description, type=type, status=status)
             return redirect('list_check', pk=new_aim.pk)
         return render(request, 'todolist_create.html', {'form': form})
 
 
-def check_list_view(request, pk):
-    try:
-        check_list = ToDoList.objects.get(pk=pk)
-    except ToDoList.DoesNotExist:
-        return HttpResponseNotFound('Задача не найдена')
-    context = {'aim_list': check_list}
-    return render(request, 'check_list.html', context)
+class CheckListView(TemplateView):
+    template_name = 'check_list.html'
+
+    def get_context_data(self, **kwargs):
+        check_list = ToDoList.objects.get(pk=kwargs.get('pk'))
+        kwargs['check_list'] = check_list
+        return super().get_context_data(**kwargs)
 
 
-def update_list_view(request, pk):
-    aim_list = get_object_or_404(ToDoList, pk=pk)
-    if request.method == 'GET':
+class UpdateView(View):
+    def get(self, request, *args, **kwargs):
+        aim_list = get_object_or_404(ToDoList, pk=kwargs.get('pk'))
         form = ToDoListForm(initial={
             'aim': aim_list.aim,
             'description': aim_list.description,
+            'type': aim_list.type,
             'status': aim_list.status,
-            'deadline_at': aim_list.deadline_at
         })
-        return render(request, 'updatelist.html', {'aim_list': aim_list, 'form':form})
-    else:
+        return render(request, 'updatelist.html', {'aim_list': aim_list, 'form': form})
+
+    def post(self, request, *args, **kwargs):
+        aim_list = get_object_or_404(ToDoList, pk=kwargs.get('pk'))
         form = ToDoListForm(data=request.POST)
         if form.is_valid():
             aim_list.aim = form.cleaned_data.get('aim')
             aim_list.status = form.cleaned_data.get('status')
-            aim_list.deadline_at = form.cleaned_data.get('deadline_at')
+            aim_list.type = form.cleaned_data.get('type')
             aim_list.description = form.cleaned_data.get('description')
-            if aim_list.deadline_at == '':
-                aim_list.deadline_at = None
             if aim_list.description == '':
                 aim_list.description = 'Отсутстувет'
             aim_list.save()
@@ -65,10 +66,13 @@ def update_list_view(request, pk):
         return render(request, 'updatelist.html', {'aim_list': aim_list, 'form': form})
 
 
-def delete_list_view(request, pk):
-    aim_list = ToDoList.objects.get(pk=pk)
-    if request.method == 'GET':
+class DeleteView(View):
+
+    def get(self, request, *args, **kwargs):
+        aim_list = get_object_or_404(ToDoList, pk=kwargs.get('pk'))
         return render(request, 'todolist_delete.html', {'aim_list': aim_list})
-    else:
+
+    def post(self, request, *args, **kwargs):
+        aim_list = get_object_or_404(ToDoList, pk=kwargs.get('pk'))
         aim_list.delete()
         return redirect('index')
