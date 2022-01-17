d67d34c370e4c6624f3caec7c18c7bfbbf3a7f33
Aaimry <wheal28@gmail.com>
2022-01-17 13:55:15 +0600

    Исправила модели типа и стотуса

--- source/todolist/models.py
+++ source/todolist/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 
-TYPE_CHOICES = [('task', 'Задача'), ('bug', 'Баг'), ('enhancement', 'Улучшение')]
-STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
-
 
 class StatusChoice(models.Model):
-    status_obj = models.CharField(max_length=11, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
+    status_obj = models.CharField(max_length=30, verbose_name='Статус')
 
     def __str__(self):
         return f"{self.status_obj}"
 
 
 class TypeChoice(models.Model):
-    type_obj = models.CharField(max_length=12, null=False, choices=TYPE_CHOICES, default='task',  verbose_name='Тип')
+    type_obj = models.CharField(max_length=30, verbose_name='Тип')
 
     def __str__(self):
         return f"{self.type_obj}"
@@ -21,10 +18,12 @@ class TypeChoice(models.Model):
 class ToDoList(models.Model):
     aim = models.CharField(max_length=200, null=False, blank=False, verbose_name='Задача')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Подробное описание')
-    type = models.ForeignKey('todolist.TypeChoice', on_delete=models.PROTECT, default='task', related_name='type', verbose_name='Тип')
-    status = models.ForeignKey('todolist.StatusChoice', on_delete=models.PROTECT, default='new', related_name='status', verbose_name='Статус')
-    create_date = models.DateTimeField(null=False, auto_now=True, verbose_name='Дата создания')
-    update_date = models.DateTimeField(null=False, auto_now=True, verbose_name='Дата редактировнаия')
+    type = models.ForeignKey('todolist.TypeChoice', on_delete=models.PROTECT, default='task', related_name='type',
+                             verbose_name='Тип')
+    status = models.ForeignKey('todolist.StatusChoice', on_delete=models.PROTECT, default='new', related_name='status',
+                               verbose_name='Статус')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата редактировнаия')
 
     def __str__(self):
         return f"{self.aim}{self.description}{self.type}{self.status}"
