37c406de03751ee31de5fe9db04b6c21de14f7bb
Aaimry <wheal28@gmail.com>
2022-01-17 17:52:35 +0600

    сделала поле type ModelMultiplyChoiceField

--- source/todolist/forms.py
+++ source/todolist/forms.py
@@ -6,5 +6,6 @@ class ToDoListForm(forms.Form):
     aim = forms.CharField(max_length=200, required=True, label='Задача')
     description = forms.CharField(max_length=2000, required=False, label='Описание',
                                   widget=forms.Textarea())
-    type = forms.ModelChoiceField(queryset=TypeChoice.objects.all(), initial='task', label='Тип')
-    status = forms.ModelChoiceField(queryset=StatusChoice.objects.all(), initial='new',  label='Статус')
+    status = forms.ModelChoiceField(queryset=StatusChoice.objects.all(), initial='new', label='Статус')
+    type = forms.ModelMultipleChoiceField(queryset=TypeChoice.objects.all(), required=False, label="Тип",
+                                          widget=forms.CheckboxSelectMultiple(attrs={'width': '30', 'height': '30'}))
