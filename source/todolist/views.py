6c5248a4e1c0af655ed6752e8020b3f02ad207d7
Aaimry <wheal28@gmail.com>
2022-01-27 15:19:35 +0600

    переписала IndexView, наследуя его от django ListView

--- source/todolist/views.py
+++ source/todolist/views.py
@@ -1,20 +1,47 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views import View
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, ListView
 from todolist.base import FormView as CustomFormView
 
-from todolist.forms import ToDoListForm
+from todolist.forms import ToDoListForm, SearchForm
 from todolist.models import ToDoList
 
 
-class IndexView(TemplateView):
-    template_name = 'todolist.html'
+class IndexView(ListView):
+    model = ToDoList
+    context_object_name = "aim_list"
+    template_name = "todolist.html"
+    paginate_by = 10
+    paginate_orphans = 0
 
-    def get_context_data(self, **kwargs):
-        aim_list = ToDoList.objects.all()
-        kwargs['aim_list'] = aim_list
-        return super().get_context_data(**kwargs)
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(aim__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset.order_by("-create_date")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = SearchForm()
+        if self.search_value:
+            context['form'] = SearchForm(initial={"search": self.search_value})
+            context['search'] = self.search_value
+        return context
+
+    def get_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get("search")
 
 
 class CreateView(CustomFormView):
