3c37d94eb9a276585cd96b85a56b2ce3370f25d7
Aaimry <wheal28@gmail.com>
2022-01-31 18:19:42 +0600

    изменила пути в urls.py с учетом проетов, а так же base.py

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from todolist.views import IndexView, CheckListView, DeleteView, UpdateView, CreateView
+from todolist.views import (
+    IndexView,
+    CheckListView,
+    DeleteView,
+    UpdateView,
+    IssuesCreateView, ProjectView, CheckProjectView, ProjectCreateView)
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('add/', CreateView.as_view(), name='list_add'),
-    path('check/<int:pk>/', CheckListView.as_view(), name='list_check'),
-    path('check/<int:pk>/update', UpdateView.as_view(), name='list_update'),
-    path('check/<int:pk>/delete', DeleteView.as_view(), name='list_delete')
+    path('', ProjectView.as_view(), name='projects'),
+    path('add/', ProjectCreateView.as_view(), name='projects_add'),
+    path('check/<int:pk>', CheckProjectView.as_view(), name='projects_check'),
+    path('projects/issues', IndexView.as_view(), name='index'),
+    path('project/<int:pk>/add/', IssuesCreateView.as_view(), name='list_add'),
+    path('project/check/<int:pk>/', CheckListView.as_view(), name='list_check'),
+    path('project/check/<int:pk>/update', UpdateView.as_view(), name='list_update'),
+    path('project/check/<int:pk>/delete', DeleteView.as_view(), name='list_delete'),
 ]
