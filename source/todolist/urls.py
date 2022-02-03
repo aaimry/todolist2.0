54a6ea3e718a6d2d4746ff5ab5a6a303daec98a6
Aaimry <wheal28@gmail.com>
2022-02-03 13:56:09 +0600

    добавила urls для всех возможных комманд

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from todolist.views import (
     IndexView,
-    CheckListView,
-    DeleteView,
-    UpdateView,
-    IssuesCreateView, ProjectView, CheckProjectView, ProjectCreateView)
+    TrackerCheckListView,
+    TrackerDeleteView,
+    TrackerUpdateView,
+    TrackerCreateView, ProjectView, CheckProjectView, ProjectCreateView, DeleteProjectView, UpdateProjectView)
 
 urlpatterns = [
     path('', ProjectView.as_view(), name='projects'),
     path('add/', ProjectCreateView.as_view(), name='projects_add'),
     path('check/<int:pk>', CheckProjectView.as_view(), name='projects_check'),
+    path('check/<int:pk>/update', UpdateProjectView.as_view(), name='projects_update'),
+    path('check/<int:pk>/delete', DeleteProjectView.as_view(), name='projects_delete'),
     path('projects/issues', IndexView.as_view(), name='index'),
-    path('project/<int:pk>/add/', IssuesCreateView.as_view(), name='list_add'),
-    path('project/check/<int:pk>/', CheckListView.as_view(), name='list_check'),
-    path('project/check/<int:pk>/update', UpdateView.as_view(), name='list_update'),
-    path('project/check/<int:pk>/delete', DeleteView.as_view(), name='list_delete'),
+    path('project/<int:pk>/add/', TrackerCreateView.as_view(), name='list_add'),
+    path('project/check/<int:pk>/', TrackerCheckListView.as_view(), name='list_check'),
+    path('project/check/<int:pk>/update', TrackerUpdateView.as_view(), name='list_update'),
+    path('project/check/<int:pk>/delete', TrackerDeleteView.as_view(), name='list_delete'),
 ]
