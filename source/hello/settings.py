310d3e6700bcd995ac88df47cbb3b4da93150944
Aaimry <wheal28@gmail.com>
2022-02-10 16:19:52 +0600

    добавили LOGOUT_REDIRECT_URL  LOGIN_REDIRECT_URL в настройки,\

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -115,6 +115,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGOUT_REDIRECT_URL = "tracker:index_project"
+LOGIN_REDIRECT_URL = "tracker:index_project"
+
+LOGIN_URL = 'accounts:login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
