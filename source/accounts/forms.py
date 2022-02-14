3fac43db31e3d29d9495cf6f9b4934b81a2961f7
Aaimry <wheal28@gmail.com>
2022-02-14 16:21:52 +0600

    добавлен метод clean_first_name

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -6,9 +6,14 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 class MyUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')
-    first_name = forms.CharField(max_length=100, required=True, label='First name')
 
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
         field_classes = {'username': UsernameField}
+
+    def clean_first_name(self):
+        data = self.cleaned_data['first_name']
+        if not data:
+            raise ValidationError('You must fill in this field')
+        return data
