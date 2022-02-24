21095f44fc3f6d8a3a804c327a30794d55d17e28
Aaimry <wheal28@gmail.com>
2022-02-24 15:52:34 +0600

    созданы UserUpdateForm, ProfileUpdateForm PasswordChangeForm

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
+from accounts.models import Profile
+
 
 class MyUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')
@@ -17,3 +19,46 @@ class MyUserCreationForm(UserCreationForm):
         last_name = cleaned_data.get('last_name')
         if first_name == '' and last_name == '':
             raise forms.ValidationError('Заполните хотя бы одно поле')
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name")
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("photo", "githubLink", "description")
+
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(label="Новый пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Старый пароль", strip=False, widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Старый пароль неправильный!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ['old_password', 'password', 'password_confirm']
