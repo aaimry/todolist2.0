f8b2bf9ca18ac1e90a629780af861879cf584c0e
Aaimry <wheal28@gmail.com>
2022-02-24 15:53:57 +0600

    зарегистрировали urls

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,10 +1,9 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view, RegisterView, UserProfileView, UserListView
+from accounts.views import RegisterView, UserProfileView, UserListView
 
+from source.accounts.views import UserPasswordChangeView, UpdateUserView
 
 app_name = 'accounts'
 
@@ -13,7 +12,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name="logout"),
     path('register/', RegisterView.as_view(), name="register"),
     path('<int:pk>/', UserProfileView.as_view(), name='user_profile'),
-    path('users/', UserListView.as_view(), name='users')
+    path('users/', UserListView.as_view(), name='users'),
+    path("update/", UpdateUserView.as_view(), name="update_user"),
+    path("change_password/", UserPasswordChangeView.as_view(), name="change_password"),
+
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
